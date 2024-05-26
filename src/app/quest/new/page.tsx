c38b67353ef6466a4a4fe27f21ffa87d232bf7a7
Rick Staa <rick.staa@outlook.com>
2024-05-26 06:08:02 +0200

    feat: add quest

--- src/app/quest/new/page.tsx
+++ src/app/quest/new/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { supabase } from "../../utils/db";
 import { useUserContext } from "@/contexts/UserContext";
+import { Button, Container, Grid, TextField, Box } from "@mui/material";
 
 const Route: React.FC = () => {
   const { userId } = useUserContext();
@@ -43,30 +44,62 @@ const Route: React.FC = () => {
   };
 
   return (
-    <div>
-      <section>
-        <h2>Update Topic and Timeframe</h2>
-        <input
-          type="text"
-          placeholder="Enter topic title"
-          value={topicTitle}
-          onChange={(e) => setTopicTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Enter topic description"
-          value={topicDescription}
-          onChange={(e) => setTopicDescription(e.target.value)}
-        />
-        <input
-          type="date"
-          placeholder="Select timeframe"
-          value={timeframe}
-          onChange={(e) => setTimeframe(e.target.value)}
-        />
-        <button onClick={handleSubmit}>Submit</button>
-      </section>
-    </div>
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="top"
+        minHeight="100vh"
+      >
+        <Grid container spacing={3} direction="column">
+          <Grid item>
+            <h2>Update Topic and Timeframe</h2>
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              type="text"
+              placeholder="Enter topic title"
+              value={topicTitle}
+              onChange={(e) => setTopicTitle(e.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              type="text"
+              placeholder="Enter topic description"
+              value={topicDescription}
+              onChange={(e) => setTopicDescription(e.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              type="date"
+              placeholder="Select timeframe"
+              value={timeframe}
+              onChange={(e) => setTimeframe(e.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{
+                fontSize: "1rem",
+                py: 2,
+                px: 2,
+                backgroundColor: "#19473f",
+              }}
+              onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
