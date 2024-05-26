edc728659f85e1b80f908efab74f2b51cd260b12
Bernardo Vieira <19441097+obernardovieira@users.noreply.github.com>
2024-05-26 03:10:41 +0200

    partially fix quests page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import CssBaseline from "@mui/material/CssBaseline";
-import { Header } from "./components/Header/Header";
 import { SnackbarProvider } from "notistack";
 import { Landing } from "./components/Landing/Landing";
-
-import { UserProvider } from "../contexts/UserContext";
 
 export default function Home() {
   return (
